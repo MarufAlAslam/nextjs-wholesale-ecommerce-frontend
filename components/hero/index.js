f17403a57b145862e8cec330e43f0af4e47dc7ec
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-09-11 08:58:42 +0600

    hero texts added

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,13 +1,23 @@
-import Image from 'next/image'
-import React from 'react'
-import hero from "../../src/assets/img/banner.webp"
+import Image from "next/image";
+import React from "react";
+import hero from "../../src/assets/img/banner.webp";
+import Link from "next/link";
 
 const Hero = () => {
-    return (
-        <div className='hero h-full'>
-            <Image src={hero} alt='hero' className='w-full h-full object-cover'/>
-        </div>
-    )
-}
+  return (
+    <div className="hero h-full relative">
+      <Image src={hero} alt="hero" className="w-full h-full object-cover" />
+      <div
+        className="overlay absolute z-10 top-0 bg-black bg-opacity-50 h-full flex flex-col justify-center items-start gap-3 p-10 w-full"
+      >
+        <h1 className="text-5xl font-bold text-white">Automobile</h1>
+        <p className="text-white">
+          We provide the best automobile services in the city
+        </p>
+        <Link href="/" className="bg-[#D1323D] text-white px-10 py-2 mt-6">Shop Now</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
